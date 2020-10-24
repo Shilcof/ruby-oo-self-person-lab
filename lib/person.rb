05c510793817af32e666fde75f32850c662a14b7
Shilcof <robert.shilcof@gmail.com>
2020-10-24 11:05:44 +0100

    commit tree 6f75c1b5d37bdb90dedf06cb80d90ec2f0086777

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
 # your code goes here
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        self.bank_account = 25
+        self.happiness = 8
+        self.hygiene = 8
+    end
+
+    def happiness=(happiness)
+        @happiness = [[happiness, 0].max, 10].min
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = [[hygiene, 0].max, 10].min
+    end
+
+    def happy?
+        happiness > 7
+    end
+
+    def clean?
+        hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        [friend, self].each {|o| o.happiness += 3 }
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        objects = [friend, self]
+        if topic == "politics"
+            objects.each {|o| o.happiness -= 2 }
+            first, second = "partisan", "lobbyist"
+        elsif topic == "weather"
+            objects.each {|o| o.happiness += 1 }
+            first, second = "sun", "rain"
+        end
+        first ||= "blah"
+        second ||= "blah"
+        "blah blah #{first} blah #{second}"
+    end
+
+end
